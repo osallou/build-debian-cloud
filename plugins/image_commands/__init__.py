87be0f3332f59857b1af2f0ef3884309113d5af9
Olivier Sallou <olivier.sallou@irisa.fr>
2013-12-13 15:34:45 +0100

    Update image_commands to match updates on base:
      fix call of ImageCommands, object should not be called, only class should be
      remove loopback_file reference, not existing anymore
      filesystem not available anymore, and depends on volume layout so remove the
      execute commands after the user_packages install, else user won't be able to
      call or update installed packages

--- plugins/image_commands/__init__.py
+++ plugins/image_commands/__init__.py
@@ -2,7 +2,7 @@
 
 def tasks(tasklist, manifest):
         from image_commands import ImageExecuteCommand
-        tasklist.add(ImageExecuteCommand())
+        tasklist.add(ImageExecuteCommand)
 
 def validate_manifest(data, schema_validate):
         from os import path
--- plugins/image_commands/image_commands.py
+++ plugins/image_commands/image_commands.py
@@ -1,21 +1,20 @@
 from base import Task
 from common import phases
+from plugins.user_packages.user_packages import AddUserPackages, \
+                                                AddLocalUserPackages
 
 
 class ImageExecuteCommand(Task):
     description = 'Execute command in the image'
     phase = phases.system_modification
+    predecessors = [AddUserPackages, AddLocalUserPackages]
 
     def run(self, info):
         from common.tools import log_check_call
-
         for user_cmd in info.manifest.plugins['image_commands']['commands']:
             command = []
             for elt in user_cmd:
-                fragment = elt.format(
-                                root=info.root,
-                                image=info.loopback_file,
-                                filesystem=info.manifest.volume['filesystem'])
+                fragment = elt.format(root=info.root)
                 command.append(fragment) 
             log_check_call(command)
 
