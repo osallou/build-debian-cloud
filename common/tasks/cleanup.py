51d1ebc0ba1dd42af29b7029eec51b61512673c7
Olivier Sallou <olivier.sallou@irisa.fr>
2013-12-13 18:27:32 +0100

    use tabs for indentation

--- common/tasks/cleanup.py
+++ common/tasks/cleanup.py
@@ -37,10 +37,10 @@ class CleanTMP(Task):
 	def run(self, info):
 		tmp = os.path.join(info.root, 'tmp')
 		for tmp_file in [os.path.join(tmp, f) for f in os.listdir(tmp)]:
-            if os.path.isfile(tmp_file):
-			    os.remove(tmp_file)
-            else:
-                shutil.rmtree(tmp_file)
+			if os.path.isfile(tmp_file):
+				os.remove(tmp_file)
+			else:
+				shutil.rmtree(tmp_file)
 
 		log = os.path.join(info.root, 'var/log/')
 		os.remove(os.path.join(log, 'bootstrap.log'))
